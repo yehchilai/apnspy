11ee77ac461f6edf453b37ce509e5d197b0c2ad6
Yeh-chi Lai <yehchilai@csupomona.edu>
2018-12-18 16:02:26 -0800

    - add config file - update .gitignore

--- apns.py
+++ apns.py
@@ -10,6 +10,7 @@
 # standard module
 import time
 import json
+import re
 # 3rd party module
 import jwt
 from hyper import HTTP20Connection
@@ -30,45 +31,62 @@ PUSH_ID = 'B102AAB758F39F1EEF9DBCC1711C7679FD61E409A749D55F9180D1E7AAB79980'
 
 class check_connection(object):
     """docstring for check_connection"""
-    def __init__(self, currentTime):
+    def __init__(self, currentTime, connect):
         super(check_connection, self).__init__()
         self.currentTime = currentTime
+        self.connect = connect
 
     def __call__(self, f):
         print 'inside check_connection __call__'
         def wrapper(*args, **kwargs):
-            print 'check connection.......'
+            print self.currentTime
+            if self.connect is not None:
+                if time.time() - self.currentTime > 3600:
+                    print time.time() - self.currentTime
+                    self.connect.connect()
             f(*args, **kwargs)
         return wrapper
 
 class APNs(object):
-    """docstring for APNs"""
-    def __init__(self, sandbox=True, team_id=None, apns_key_id=None, bundle_id=None, key_file=None):
-        super(APNs, self).__init__()
-        self._team_id = team_id
-        self._kid = apns_key_id
-        self._algorithm = 'ES256'
-        self._init_time = time.time()
-        with open(key_file) as f:
-            self._secret = f.read()
-        self._token = jwt.encode({
-                                   'iss': self._team_id,
-                                   'iat': self._init_time
-                                 },
-                                 self._secret,
-                                 algorithm=self._algorithm,
-                                 headers={
-                                   'alg': self._algorithm,
-                                   'kid': self._kid
-                                })
-        self._bundle_id = bundle_id
-        if sandbox:
-            self._apns_server = 'api.development.push.apple.com:443'
-        else:
-            self._apns_server = 'api.push.apple.com:443'
-        self._connect = HTTP20Connection(self._apns_server, force_proto='h2')
+    """
+    """
+    apns_connect = None
+    apns_time = None
 
-    @check_connection(time.time())
+    def __init__(self, sandbox=True, config_file=None):
+        super(APNs, self).__init__()
+
+        self.__algorithm = 'ES256'
+        self.__init_time = time.time()
+        with open(config_file) as cfg:
+            input_str = re.sub(r'\\\n', '', cfg.read())
+            input_str = re.sub(r'//.*\n', '\n', input_str)
+            data = json.loads(input_str)
+            self.__team_id = data["team_id"]
+            self.__kid = data["apns_key_id"]
+            self.__app_id = data["app_id"]
+            self.__key_file = data["key_file"]
+
+        with open(self.__key_file) as f:
+            self.__secret = f.read()
+        self.__token = jwt.encode({
+                                   'iss': self.__team_id,
+                                   'iat': self.__init_time
+                                 },
+                                 self.__secret,
+                                 algorithm=self.__algorithm,
+                                 headers={
+                                   'alg': self.__algorithm,
+                                   'kid': self.__kid
+                                })
+
+        if sandbox:
+            self.__apns_server = 'api.development.push.apple.com:443'
+        else:
+            self.__apns_server = 'api.push.apple.com:443'
+        APNs.apns_connect = HTTP20Connection(self.__apns_server, force_proto='h2')
+
+    # @check_connection(apns_time, apns_connect)
     def send(self, message, sound, badges, device_token=None, content_available=False):
         """
         """
@@ -79,8 +97,8 @@ class APNs(object):
         request_headers = {
             'apns-expiration': '0',
             'apns-priority': '10',
-            'apns-topic': self._bundle_id,
-            'authorization': 'bearer {0}'.format(self._token.decode('ascii'))
+            'apns-topic': self.__app_id,
+            'authorization': 'bearer {0}'.format(self.__token.decode('ascii'))
         }
         if content_available:
             content = 1
@@ -89,29 +107,27 @@ class APNs(object):
         payload_data = {
             'aps': {
                 'alert' : message,
-                # This is silent push
                 'sound' : sound,
                 'badge' : badges,
-                # Key to silent push
                 'content-available': content
             }
         }
         payload = json.dumps(payload_data).encode('utf-8')
-        self._connect.request(
+        APNs.apns_connect.request(
             'POST',
             path,
-            payload,
+            body=payload,
             headers=request_headers
         )
 
         # https://github.com/genesluder/python-apns/pull/3
         # http://www.ehowstuff.com/how-to-install-and-update-openssl-on-centos-6-centos-7/
-        resp = self._connect.get_response()
+        resp = APNs.apns_connect.get_response()
         print(resp.status)
         print(resp.read())
 
 
-apns = APNs(sandbox=True, team_id=TEAM_ID, apns_key_id=APNS_KEY_ID, bundle_id=BUNDLE_ID, key_file=APNS_AUTH_KEY)
+apns = APNs(sandbox=True, config_file="myconfig.json")
 # apns.send('This is a test 2', 'default', 2, device_token=PUSH_ID, content_available=True)
 while True:
     message = raw_input('Enter the message: ')
